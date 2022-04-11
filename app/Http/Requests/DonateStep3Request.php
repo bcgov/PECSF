ad5afec9a452e58c694c33fa9e44016e8167de6c
Kousthubha-M <74929858+Kousthubha-M@users.noreply.github.com>
2022-04-11 14:59:53 -0400

    Fix error msg for negative value

--- app/Http/Requests/DonateStep3Request.php
+++ app/Http/Requests/DonateStep3Request.php
@@ -40,7 +40,7 @@ class DonateStep3Request extends FormRequest
     public function messages()
     {
         return [
-            "*.*.gt" => 'Check your donation allocation. Ensure your totals are correct and there are no negative numbers entered'
+            "*.*.gte" => 'Check your donation allocation. Ensure your totals are correct and there are no negative numbers entered'
         ];
     }
 }
