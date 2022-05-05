599e1b321f807fa672b80c4fc72780ef824fd30b
jizhaogit <ji.zhao@telus.com>
2022-05-05 13:40:00 -0400

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -10,52 +10,61 @@
         <div class="row">
             <div class="col">
                 <div class="card">
+                <a href="/pledge">
                     <div class="card-body mt-4 text-center">
                         <div>
-                            <img src="{{asset('img/admin/2.png')}}" alt="" style="height:100px">
+                            <img src="{{asset('img/admin/2.png')}}" alt="Pledge Administration" style="height:100px">
                         </div>
                         Pledge Administration <br>
                         <i class="fas fa-arrow-right"></i>
                     </div>
+                </a>   
                 </div>
+                 
             </div>
 
-            <div class="col">
-                <div class="card">
+            <div class="col">                
+                <div class="card">    
+                    <a href="/campaignyears">
                     <div class="card-body mt-4  text-center">
                         <div>
-                            <img src="{{asset('img/admin/1.png')}}" alt="" style="height:100px">
+                            <img src="{{asset('img/admin/1.png')}}" alt="Campaign Set-up" style="height:100px">
                         </div>
                         Campaign Set-up<br>
                         <i class="fas fa-arrow-right"></i>
                     </div>
+                    </a>    
                 </div>
             </div>
         </div>
 
         <div class="row">
-            <div class="col">
+            <div class="col">                
                 <div class="card">
+                    <a href="/settings/regions">
                     <div class="card-body mt-4  text-center">
                         <div>
-                            <img src="{{asset('img/admin/4.png')}}" alt="" style="height:100px">
+                            <img src="{{asset('img/admin/4.png')}}" alt="Training, Communications and Engagement" style="height:100px">
                         </div>
                         Training, Communications and Engagement<br>
                         <i class="fas fa-arrow-right"></i>
                     </div>
+                    </a>
                 </div>
             </div>
 
             <div class="col">
                 <div class="card">
+                    <a href="/reporting">
                     <div class="card-body mt-4  text-center">
                         <div>
-                            <img src="{{asset('img/admin/3.png')}}" alt="" style="height:100px">
+                            <img src="{{asset('img/admin/3.png')}}" alt="Reporting" style="height:100px">
                         </div>
                         Reporting<br>
                         <i class="fas fa-arrow-right"></i>
                     </div>
-                </div>
+                    </a>  
+                </div>  
             </div>
         </div>
     </div>
