67839746f126a11231db5a2aab09302deada0afc
james <jpoon88@yahoo.com>
2022-06-02 11:28:12 -0700

    Update CampaignYearSeeder to assign 27 periods for year 2005 and 2027

--- database/seeders/CampaignYearSeeder.php
+++ database/seeders/CampaignYearSeeder.php
@@ -17,14 +17,19 @@ class CampaignYearSeeder extends Seeder
     public function run()
     {
 
-        for ($x = 1990; $x <= 2030; $x++) {
-        
+        for ($x = 2005; $x <= 2030; $x++) {
+          
+          $periods = 26;
+          if ($x == 2015 or $x == 2027) {
+              $periods = 27;
+          }
+
           CampaignYear::updateOrCreate([
             'calendar_year' => $x,
           ],[  
             'calendar_year' => $x,
-            'number_of_periods' =>  26,
-            'status' => 'I',
+            'number_of_periods' => $periods,
+            'status' => $x == 2023 ? 'A' : 'I',
             'start_date' => Carbon::create($x -1, '09', '01'),
             'end_date' => Carbon::create($x -1, '11', '30'),
             'close_date' => Carbon::create($x , '12', '31'),
