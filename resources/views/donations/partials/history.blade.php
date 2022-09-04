3cdf93884228911143f8ced46682e07133307461
TELUS\x257354 <paulchester.george@telus.com>
2022-09-04 04:04:13 -0400

    duplicate button logic

--- resources/views/donations/partials/history.blade.php
+++ resources/views/donations/partials/history.blade.php
@@ -53,7 +53,7 @@
                     @endforeach
                 </table>
 
-                @if($key >= $currentYear || $ignore)
+                @if($key > $currentYear || $ignore)
                     @php
                         $ignore = true;
                     @endphp
