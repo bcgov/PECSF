02d7d757717981a155f1e68a62df05f3739ff1e2
TELUS\x257354 <paulchester.george@telus.com>
2022-09-09 11:44:38 -0400

    tab selection donor selection

--- resources/views/volunteering/partials/organizations.blade.php
+++ resources/views/volunteering/partials/organizations.blade.php
@@ -9,17 +9,17 @@
     @foreach($organizations as $key => $organization)
 
 
-        <tr tabindex="{{$key+3}}">
+        <tr >
             <td style="width:70%;"><b>{{$organization->charity_name}}</b><br>{{$organization::CATEGORY_LIST[$organization->category_code]}} | {{$organization->city}} | {{$organization->province}} | {{$organization->country}}</td>
             <td class="blue" style="width:15%"><span class="view_details" registration_number="{{$organization->registration_number}}" charity_status="{{$organization->charity_status}}" effective_date_of_status="{{$organization->effective_date_of_status}}" sanction="{{$organization->sanction}}" designation="{{$organization->designation}}" category="{{$organization->category}}" address="{{$organization->address}}" city="{{$organization->city}}" province="{{$organization->province}}" country="{{$organization->country}}" postal="{{$organization->postal_code}}" website="{{$organization->uri}}" charitable_programs=""><b><u>View Details</u></b></span></td>
-            <td style="width:5%"><button style="width:100px;" class="
+            <td style="width:5%"><button style="width:100px;" type="button" class="
             @php
             if(in_array($organization->id,$selected_vendors))
                 {
-                    echo "selected active";
+                    echo "selected active ";
                 }
             else{
-                echo "select";
+                echo "select ";
             }
             @endphp
           btn btn-outline-primary" name="{{$organization->charity_name}}" org_id="{{$organization->id}}">
