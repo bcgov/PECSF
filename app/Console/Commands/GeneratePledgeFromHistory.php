ef1f5ee78ef261fc2c8cd9146d8ad00d165895a9
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2022-12-05 10:51:31 -0800

    S33(16) FMO/BI - Load historical 2022 bolt-on data into Greenfield - Part 2

--- app/Console/Commands/GeneratePledgeFromHistory.php
+++ app/Console/Commands/GeneratePledgeFromHistory.php
@@ -547,7 +547,7 @@ class GeneratePledgeFromHistory extends Command
                     'special_program' => $bi_pledge->name2,
 
                     'ods_export_status' => 'C',
-                    'ods_export_at' => $this->yearcd . '-12-31',
+                    'ods_export_at' => $this->yearcd . '-12-31 00:00:00',
 
                     'created_at' => $bi_pledge->created_date,
                 ]);
@@ -862,7 +862,7 @@ class GeneratePledgeFromHistory extends Command
                     'goal_amount' => $bi_pledge->goal_amount,
 
                     'ods_export_status' => 'C',
-                    'ods_export_at' => $this->yearcd . '-12-31',
+                    'ods_export_at' => $this->yearcd . '-12-31 00:00:00',
 
                     'created_at' => $bi_pledge_detail->created_date,
                 ]); 
