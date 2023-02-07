269c005d0395d758d6610ca5dc9666f18e0987b3
TELUS\x257354 <paulchester.george@telus.com>
2023-02-07 15:49:46 -0500

    S36(16) Dev - Volunteer/Admin supply order

--- resources/views/volunteering/partials/supply.blade.php
+++ resources/views/volunteering/partials/supply.blade.php
@@ -1,2 +1,126 @@
 Supply Here
-<?php
+<div class="card" style="padding:50px;">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-primary">PECSF Supply Order Form</h1>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <label class="text-primary">Supplies</label>
+        </div>
+    </div>
+
+    <div class="row" style="background:#f7f7f7;border-radius:5px;padding:25px;">
+        <label class="col-md-4">
+            Calendars
+            <input name="calendars" class=" form-control" />
+        </label>
+        <label class="col-md-4">
+            Campaign Posters
+            <input name="campaign_posters" class=" form-control" />
+        </label>
+        <label class="col-md-4">
+            Stickers
+            <input name="stickers" class=" form-control" />
+        </label>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <label class="text-primary">Mailing Information</label>
+        </div>
+    </div>
+
+    <div class="row" style="background:#f7f7f7;border-radius:5px;padding:25px;">
+        <label class="col-md-4">
+            First Name
+            <input name="calendars" class=" form-control" />
+        </label>
+        <label class="col-md-4">
+            Last Name
+            <input name="campaign_posters" class=" form-control" />
+        </label>
+        <label class="col-md-4">
+            Organization
+            <input name="stickers" class=" form-control" />
+        </label>
+    </div>
+
+    <div class="row" style="padding:25px;">
+        <h2>Please select a mailing option below:</h2>
+    </div>
+
+
+    <div class="row" style="position:relative;background:#f7f7f7;border-radius:5px;padding:25px;">
+        <label style="left:0px;top:-25px;position:absolute;padding:8px;border-top-left-radius: 5px;border-top-right-radius: 5px;" class="col-md-12 bg-blue">
+            Mailing Address
+        </label>
+        <label class="col-md-4">
+            Unit/Suite/Floor
+            <input name="unit_suite_floor" class="form-control" />
+        </label>
+        <label class="col-md-4">
+            Physical address (street # and name)
+            <input name="physical_address" class="form-control" />
+        </label>
+        <label class="col-md-4">
+            City
+            <input name="city" class="form-control" />
+        </label>
+        <label class="col-md-6">
+            Province
+            <input name="province" class="form-control" />
+        </label>
+        <label class="col-md-6">
+            Postal Code
+            <input name="postal_code" class="form-control" />
+        </label>
+    </div>
+
+    <div class="row" style="position:relative;background:#f7f7f7;border-radius:5px;padding:25px;">
+        <label style="left:0px;top:-25px;position:absolute;padding:8px;border-top-left-radius: 5px;border-top-right-radius: 5px;" class="col-md-12 bg-blue">
+            PO Box Address
+        </label>
+        <label class="col-md-12">
+            Po Box
+            <input name="po_box" class="form-control" />
+        </label>
+        <label class="col-md-4">
+            City
+            <input name="city" class="form-control" />
+        </label>
+        <label class="col-md-4">
+            Province
+            <input name="province" class="form-control" />
+        </label>
+        <label class="col-md-4">
+            Postal Code
+            <input name="postal_code" class="form-control" />
+        </label>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <label class="text-primary">Additional Information</label>
+        </div>
+    </div>
+    <div class="row" style="background:#f7f7f7;border-radius:5px;padding:25px;">
+        <label class="col-md-6">
+            Date Required
+            <input name="date_required" type="date" class=" form-control" />
+        </label>
+        <label class="col-md-6">
+            Comments
+            <input name="comments" class=" form-control" />
+        </label>
+    </div>
+</div>
+
+<div class="card">
+    
+</div>
+
+
+
