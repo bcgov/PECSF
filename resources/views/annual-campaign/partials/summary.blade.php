1b73b0b576f072774f1937814a09ad353d99833a
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2023-02-07 09:23:25 -0800

    Bug - Message to go back and edit the pledge details is not displayed while duplicating a pledge

--- resources/views/annual-campaign/partials/summary.blade.php
+++ resources/views/annual-campaign/partials/summary.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-12">
             <h3 class="mt-0">{{ $pool_option == 'C' ?  '5. Summary' : '4. Summary' }}</h3>
-            <p class="mt-3">Please review your donation plan and press <b>Pledge</b> when ready!</p>
+            <p class="mt-3">Please review your donation plan and press <b>“Pledge”</b> when ready! Use the "Back” button, to make changes to your pledge.</p>
                 <div class="card bg-light p-3">
                     <p class="card-title"><b>Deductions</b></p>
                     <div class="card">
