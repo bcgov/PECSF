03ba9f22f12886233fcdbbd38307b76619ea1d0f
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2023-03-04 14:22:11 -0800

    New feature -- to have auditing capabilities for models to keep track of changes made to records, as well as any changes to their attributes

--- app/Models/Charity.php
+++ app/Models/Charity.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Charity extends Model
+class Charity extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
     protected $fillable = [
         'registration_number',
