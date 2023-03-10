7fba8b4bc21f4647e80bcaffd2510485b0366d50
Kunal Kapoor <kunal@freshworks.io>
2023-03-10 10:20:08 -0800

    Fix possible timing issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 /usr/local/bin/apache2-foreground
 
+wait
+
 cd /var/www/html
 
-touch abc.txt
 
 nohup php artisan queue:work --tries=3 --timeout=0 --memory=512 > ./storage/logs/queue-work-1.log &
-echo $?
