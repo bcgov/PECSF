0775c222c7e800e02214cd80a4713e4c56cafd56
Kunal Kapoor <kunal@freshworks.io>
2023-03-12 17:55:18 -0700

    Move queue from entrypoint to deployment config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
 /usr/local/bin/apache2-foreground
-
-wait
-
-echo "Apache started" >> /var/www/html/storage/logs/apache-dummy.log
-
-cd /var/www/html
-
-
-nohup php artisan queue:work --tries=3 --timeout=0 --memory=512 > ./storage/logs/queue-work-1.log &
