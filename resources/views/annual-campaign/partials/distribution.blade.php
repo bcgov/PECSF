4cffba3d5919ba2440cfca3f05de3039d6fdff24
TELUS\x257354 <paulchester.george@telus.com>
2023-04-03 10:11:18 -0400

    Donations: Interchange the Percentage & Dollar amount switch content on Step 5 - Summary

--- resources/views/annual-campaign/partials/distribution.blade.php
+++ resources/views/annual-campaign/partials/distribution.blade.php
@@ -4,13 +4,13 @@
 
 @foreach(['one-time', 'bi-weekly'] as $key)
     {{-- @if($key === 'one-time' && (session()->get('amount-step')['frequency'] === 'one-time' || session()->get('amount-step')['frequency'] === 'both')) --}}
-    @if($key === 'one-time' && ($frequency === 'one-time' || $frequency === 'both')) 
+    @if($key === 'one-time' && ($frequency === 'one-time' || $frequency === 'both'))
         @php $key_ = $key; @endphp
         @php $keyCase = 'oneTime'; @endphp
         @include('annual-campaign.partials.amount-distribution')
     @endif
     {{-- @if($key === 'bi-weekly' && (session()->get('amount-step')['frequency'] === 'bi-weekly' || session()->get('amount-step')['frequency'] === 'both')) --}}
-    @if($key === 'bi-weekly' && ($frequency === 'bi-weekly' || $frequency === 'both'))     
+    @if($key === 'bi-weekly' && ($frequency === 'bi-weekly' || $frequency === 'both'))
         @php $key_ = $key;@endphp
         @php $keyCase = 'biWeekly'; @endphp
         @include('annual-campaign.partials.amount-distribution')
