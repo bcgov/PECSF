f734f733092912b3df3b6dc6e432e528b8f03a97
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2023-04-10 09:48:21 -0700

    Improve User Experience by repositioning 'processing' status on the top of the datatables and also showing aninated process icon

--- resources/views/system-security/export-audit-log/index.blade.php
+++ resources/views/system-security/export-audit-log/index.blade.php
@@ -204,6 +204,10 @@
         margin-bottom: 10px;
     }
 
+    div.dataTables_wrapper div.dataTables_processing {
+      top: 5%;
+    }
+
 </style>
 @endpush
 
@@ -232,6 +236,9 @@
             retrieve: true,
             "searching": true,
             processing: true,
+            "language": {
+               processing: '<i class="fa fa-spinner fa-pulse fa-3x fa-fw text-info"></i><span class="sr-only">Loading...</span>'
+            },
             serverSide: true,
             // select: true,
             'order': [[ 0, 'desc']],
--- resources/views/system-security/schedule-job-audits/index.blade.php
+++ resources/views/system-security/schedule-job-audits/index.blade.php
@@ -164,6 +164,10 @@
         margin-bottom: 10px;
     }
 
+    div.dataTables_wrapper div.dataTables_processing {
+      top: 1%;
+    }
+
 </style>
 @endpush
 
@@ -192,6 +196,9 @@
             retrieve: true,
             "searching": true,
             processing: true,
+            "language": {
+               processing: '<i class="fa fa-spinner fa-pulse fa-3x fa-fw text-info"></i><span class="sr-only">Loading...</span>'
+            },
             serverSide: true,
             // select: true,
             'order': [[ 0, 'desc']],
