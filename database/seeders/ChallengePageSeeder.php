5ae27a49619320c23adcc0b942bf01ae9cab2f29
TELUS\x257354 <paulchester.george@telus.com>
2023-04-17 13:04:21 -0400

    Remove seeder updates

--- database/seeders/ChallengePageSeeder.php
+++ database/seeders/ChallengePageSeeder.php
@@ -16,23 +16,20 @@ class ChallengePageSeeder extends Seeder
      */
     public function run()
     {
-        BusinessUnit::where("code", "GCPE")
+      /*  BusinessUnit::where("code", "GCPE")
             ->update(['code' => "BC022"]);
         BusinessUnit::where("code", "EMBC")
             ->update(['code' => "BC010"]);
 
-        /*Ministry of Attorney General (105) gets combined with Ministry of Housing (131). */
         BusinessUnit::where("code", "BC131")
             ->update(['code' => "BC105"]);
 
-        /*Ministry of Citizens’ Services (112) get combined with Product Services (067) */
         BusinessUnit::where("code", "BC067")
             ->update(['code' => "BC112"]);
 
-        /*Ministry of Education and Child Care (062) gets combined with Teacher’s Special Account (063). */
         BusinessUnit::where("code", "BC063")
             ->update(['code' => "BC062"]);
-
+*/
 
 
 
