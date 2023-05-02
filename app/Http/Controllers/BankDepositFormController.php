63c254a50844a1c2528952a19f49ceb7ef9a3078
TELUS\x257354 <paulchester.george@telus.com>
2023-05-01 22:51:39 -0400

    Include image for fsps

--- app/Http/Controllers/BankDepositFormController.php
+++ app/Http/Controllers/BankDepositFormController.php
@@ -605,6 +605,7 @@ class BankDepositFormController extends Controller
             $organizations->whereIn('charities.id', $pool->charities->pluck('charity_id') );
             $organizations->join('f_s_pool_charities',"charities.id","f_s_pool_charities.charity_id");
             $organizations->where("f_s_pool_charities.status","=","A");
+            $organizations->selectRaw("image");
         }
 
         $organizations = $organizations->where("charity_status","=","Registered")->groupby("charity_name")->paginate(7);
