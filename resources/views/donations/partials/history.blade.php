b771d9cc7236937a2763f9949da01408e29a130d
techmusicmoney <131987472+techmusicmoney@users.noreply.github.com>
2023-07-25 08:16:56 -0400

    debug code

--- resources/views/donations/partials/history.blade.php
+++ resources/views/donations/partials/history.blade.php
@@ -107,6 +107,7 @@
                                             data-frequency="{{ $pledge->frequency }}"
                                             data-yearcd="{{ $pledge->yearcd }}">Details
                                 </button>
+                                {{var_dump($pledge)}}
                                 {{-- @endif --}}
                             </td>
                         </tr>
