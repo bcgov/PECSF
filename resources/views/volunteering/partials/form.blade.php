de1649e55ef6469d9daad655a1235b75bb8880ab
jizhaogit <ji.zhao@telus.com>
2023-10-18 10:43:15 -0400

    [jz-009]Admin -> Event Submission Queue issues

--- resources/views/volunteering/partials/form.blade.php
+++ resources/views/volunteering/partials/form.blade.php
@@ -87,7 +87,7 @@
         <div class="form-group col-md-3 sub_type">
             <label for="sub_type">Sub type</label>
             <select class="form-control" type="text" id="sub_type" name="sub_type">
-                <option value="false">Disabled</option>
+                <option value="none">Disabled</option>
             </select>
             <span class="sub_type_errors errors">
                        @error('form_submitter')
