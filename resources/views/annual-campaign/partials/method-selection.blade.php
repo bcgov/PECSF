e9b6100768b0dab8e56645bcef22a4c7275efe78
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2024-02-10 20:47:42 -0800

    [jp-0100+jp-0101] Dev - Donations Flow Style Changes

--- resources/views/annual-campaign/partials/method-selection.blade.php
+++ resources/views/annual-campaign/partials/method-selection.blade.php
@@ -1,11 +1,11 @@
 <div id="method-selection-area">
     <p class="p-1"></p>
 
-    <div class="card btn btn-outline-primary text-left {{ $pool_option == "C" ? 'active' : '' }}" id="card-pool1">
+    <div class="card btn btn-outline-primary text-left {{ $pool_option == "C" ? 'active' : '' }}" id="card-pool1" tabindex="0">
         <div class="card-body p-2 ">
             <div class="form-check ">
                 <input class="form-check-input" type="radio" name="pool_option" id="pool1" value="C"
-                    {{ $pool_option == "C" ? 'checked' : '' }}>
+                    {{ $pool_option == "C" ? 'checked' : '' }}  tabindex="-1">
                 <label style="font-size:16px;line-height:25.6px;" class="form-check-label" for="pool1">
                     <strong>Select up to 10 charities from the CRA List</strong><br>
                     <span>Explore charities by keyword, category, province, or view charities, and their associated programs, which are part of the Fund Supported Pools.
@@ -15,11 +15,11 @@
         </div>
     </div>
 
-    <div class="card btn btn-outline-primary text-left {{ $pool_option == "P" ? 'active' : '' }}" id="card-pool2">
+    <div class="card btn btn-outline-primary text-left {{ $pool_option == "P" ? 'active' : '' }}" id="card-pool2" tabindex="0">
         <div class="card-body p-2">
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="pool_option" id="pool2" value="P"
-                    {{ $pool_option == "P" ? 'checked' : '' }}>
+                    {{ $pool_option == "P" ? 'checked' : '' }}  tabindex="-1">
                 <label style="font-size:16px;line-height:25.6px;" class="form-check-label h5" for="pool2">
                     <strong>Select a regional Fund Supported Pool</strong><br>
                     <span>Charities and distribution amounts are pre-determined.</span>
@@ -46,6 +46,15 @@ $(function () {
             $('ul.bs4-step-tracking li.amount-distribution').show();
         }
     }
+
+    // Enter or space key on Wizard STEP icon to forward and backward 
+    $('#method-selection-area .card').on('keyup', function(e) {
+        var key  = e.key;
+        if (key === ' ' || key === 'Enter') {
+            e.preventDefault();
+            $(this).trigger('click');
+        }
+    });
 
     $('#method-selection-area .card').click( function(event) {
         // var current_id = event.target.id;
