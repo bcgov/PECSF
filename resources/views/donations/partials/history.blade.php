0b2f827e2c792c06b3d48dc241bb17861335eceb
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2024-02-14 05:51:53 -0800

    [jp-0101] Dev - Donation history change amount to total amount (additional change on expand/collapse icon

--- resources/views/donations/partials/history.blade.php
+++ resources/views/donations/partials/history.blade.php
@@ -13,7 +13,7 @@
                 </span>
                 <div class="flex-fill"></div>
                 <button class="btn btn-link btn-nav-accordion {{ $loop->index == 0 ? 'collapsed' : ''}}" type="button" data-id="{{ $key }}">
-                    <i class="fas fa-chevron-up fa-lg"></i>
+                    <i class="fas fa-angle-down fa-2x"></i>
                 </button>
                 {{-- <button class="custom-expander btn btn-primary" data-id="{{ $key }}">
                     @if ($loop->index == 0)
