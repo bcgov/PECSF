e5d6801198a7250b5a63db7f2930ee5602b6ac05
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2024-04-29 11:51:34 -0700

    [jp-0125h] Volunteer Admin Part 2 (volunteer profile report group under program reports section)

--- resources/views/admin-volunteering/partials/tabs.blade.php
+++ resources/views/admin-volunteering/partials/tabs.blade.php
@@ -1,17 +1,9 @@
 <ul class="menu nav nav-pills" id="pills-tab">
 
-    <li class="nav-item dropdown">
-        @php $active =  ( str_contains(Route::current()->getName(), 'admin-volunteering.profile') ||
-                          str_contains(Route::current()->getName(), 'TBA') 
-                        ) ? 'active' : ''
-        @endphp
-        <a class="nav-link dropdown-toggle {{ $active }}" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Registration</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item {{ str_contains( Route::current()->getName(), 'admin-volunteering.profile') ? 'active' : ''}}"
-                href="{{ route('admin-volunteering.profile.index') }}">Maintain Volunteer Profile</a>
-          <a class="dropdown-item {{ str_contains( Route::current()->getName(), 'TBA') ? 'active' : ''}}"
-                href="{{ '' }}">Volunteer Profile Report</a>
-        </div>
+    <li class="nav-item nav-center-3">        
+        <a class="nav-link {{ str_contains( Route::current()->getName(), 'admin-volunteering.profile') ? 'active' : ''}}"
+            id="pills-home-tab" href="{{ route('admin-volunteering.profile.index') }}" role="tab"
+            aria-controls="pills-home" aria-selected="true">Maintain Volunteer</a>
     </li>
 
     <li class="nav-item nav-center-3">
