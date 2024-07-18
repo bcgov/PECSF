094596a286a43c80b8838fd5d42c83d5036317be
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2024-07-18 07:43:06 -0700

    [jp-0160] Avoid charity add on the list when beyond the maximum limit

--- resources/views/donate-now/wizard.blade.php
+++ resources/views/donate-now/wizard.blade.php
@@ -278,9 +278,9 @@
     }
 
     /* Temporaily override the app.scss*/
-    .btn:focus, button:focus{
+    /* .btn:focus, button:focus{
         border: #fff 2px solid!important;
-    }
+    } */
 
     .custom.pagination .page-item {
         margin-top: 0;
